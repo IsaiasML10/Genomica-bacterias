5a7e09d707ff638008a2a2aaa1210481c1c2b3af
IsaiasML10 <112969083+IsaiasML10@users.noreply.github.com>
2023-06-26 11:15:14 -0500

    Update Depth.sh
    Script para obtener la profundidad (depth) y cobertura (coverage) a partir de un archivo .bam (función de bbmap)

--- Depth.sh
+++ Depth.sh
@@ -2,3 +2,4 @@
 bbmap.sh ref=referencia in=input1 in2=input2 minid=0.85 out=out.bam -Xmx12g
 #Para ver profundidad y coverage usando el .bam generado mediante map to reference (función de bbmap)
 pileup.sh in=out.bam out=depth.txt 
+ 
