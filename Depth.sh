736e1dfbf5897ec05c25387cc37ebad82f308056
IsaiasML10 <112969083+IsaiasML10@users.noreply.github.com>
2023-06-26 10:55:59 -0500

    Create Depth.sh

---
+++ Depth.sh
@@ -0,0 +1,4 @@
+#map to reference 
+bbmap.sh ref=referencia in=input1 in2=input2 minid=0.85 out=out.bam -Xmx12g
+#Para ver profundidad y coverage usando el .bam generado mediante map to reference
+pileup.sh in=out.bam out=depth.txt
