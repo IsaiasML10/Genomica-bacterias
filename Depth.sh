9de2a703d85cc48d6b2e051e7121f87c26c75fd5
IsaiasML10 <112969083+IsaiasML10@users.noreply.github.com>
2023-06-26 11:14:08 -0500

    Update Depth.sh

--- Depth.sh
+++ Depth.sh
@@ -1,4 +1,4 @@
-#map to reference 
+#map to reference usando bbmap
 bbmap.sh ref=referencia in=input1 in2=input2 minid=0.85 out=out.bam -Xmx12g
-#Para ver profundidad y coverage usando el .bam generado mediante map to reference
+#Para ver profundidad y coverage usando el .bam generado mediante map to reference (función de bbmap)
 pileup.sh in=out.bam out=depth.txt 
